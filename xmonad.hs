748d5f3e2b8da48c9601309ba8daba6512ca85d4
Mike Sampson <mike@sambodata.com>
2009-11-29 14:17:11 +1100

    Clean up long lines and add screen number detection for workspace sorting.

--- xmonad.hs
+++ xmonad.hs
@@ -1,38 +1,47 @@
 -- xmonad.hs
 -- Mike Sampson
 
-import XMonad hiding ( (|||) )
-import XMonad.Layout.LayoutCombinators
-import XMonad.Operations
-import XMonad.Actions.DwmPromote
-import XMonad.Actions.GridSelect
-import XMonad.Actions.Search
-import XMonad.Actions.Submap
-import XMonad.Hooks.DynamicLog   ( PP(..), dynamicLogWithPP, dzenColor, wrap, defaultPP )
-import XMonad.Hooks.SetWMName
-import XMonad.Layout hiding ( (|||) )
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Grid
-import XMonad.Layout.Tabbed
-import XMonad.Layout.LayoutHints
-import XMonad.Prompt             ( XPConfig(..), XPPosition(..), defaultXPConfig )
-import XMonad.Prompt.Shell       ( shellPrompt )
-import XMonad.Prompt.AppendFile  ( appendFilePrompt )
-import XMonad.Util.Run
-import XMonad.Util.WorkspaceCompare ( getSortByXineramaRule )
-import XMonad.Util.Scratchpad
-import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.FadeInactive
+-- General
 import Data.List
-import Text.Printf
-import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 import Data.Bits ((.|.))
 import Data.Ratio
 import Graphics.X11
-
+import Text.Printf
 import System.IO
+-- XMonad Core
+import XMonad hiding ( (|||) )
+import XMonad.Operations
+import qualified XMonad.StackSet as W
+-- XMonad.Actions
+import XMonad.Actions.DwmPromote
+import XMonad.Actions.GridSelect
+import XMonad.Actions.Search
+import XMonad.Actions.Submap
+-- XMonad.Hooks
+import XMonad.Hooks.DynamicLog ( PP(..),
+        dynamicLogWithPP, dzenColor, wrap, defaultPP )
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.FadeInactive
+-- XMonad.Layout
+import XMonad.Layout hiding ( (|||) )
+import XMonad.Layout.LayoutCombinators
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Grid
+import XMonad.Layout.Tabbed
+import XMonad.Layout.LayoutHints
+import XMonad.Layout.IndependentScreens (countScreens)
+-- XMonad.Prompt
+import XMonad.Prompt ( XPConfig(..), XPPosition(..), defaultXPConfig )
+import XMonad.Prompt.Shell ( shellPrompt )
+import XMonad.Prompt.AppendFile ( appendFilePrompt )
+-- XMonad.Util
+import XMonad.Util.Run
+import XMonad.Util.WorkspaceCompare ( getSortByIndex, getSortByXineramaRule )
+import XMonad.Util.Scratchpad
+
 
 color0 :: String
 color0 = "#0390e1" -- blue
@@ -43,13 +52,13 @@ font0 :: String
 font0 = "-windows-proggyclean-medium-r-normal--13-80-96-96-c-70-iso8859-1"
 
 statusBarCmd :: String
-
-statusBarCmd =  printf "%s -bg '%s' -fg '%s' -w %d -sa c -fn '%s' -e '' -x 0 -ta l"
-                       "/usr/local/bin/dzen2"
-                       "black"
-                       "grey30"
-                       (1380::Integer)
-                       font0
+statusBarCmd =  printf
+    "%s -bg '%s' -fg '%s' -w %d -sa c -fn '%s' -e '' -x 0 -ta l"
+    "/usr/local/bin/dzen2"
+    "black"
+    "grey30"
+    (1380::Integer)
+    font0
 
 myWorkspaces :: [String]
 myWorkspaces = map (\(x, y) -> show x ++ ":" ++ y)
@@ -57,6 +66,7 @@ myWorkspaces = map (\(x, y) -> show x ++ ":" ++ y)
 
 main = do
     din <- spawnPipe statusBarCmd
+    sc <- countScreens
     xmonad $ defaultConfig
         { borderWidth        = 1
         , normalBorderColor  = "grey30"
@@ -66,26 +76,28 @@ main = do
         , modMask            = mod4Mask
         , manageHook         = myManageHook <+> scratchpadManageHookDefault
         , startupHook        = startup
-        , logHook            = dynamicLogWithPP $ myPP din
-        , layoutHook         = avoidStruts ( layoutHints (smartBorders tiled ||| Mirror tiled ||| noBorders Full ||| Grid))
+        , logHook            = dynamicLogWithPP $ myPP din sc
+        , layoutHook         = avoidStruts ( layoutHints
+              (smartBorders tiled ||| Mirror tiled ||| noBorders Full ||| Grid))
         , keys               = \c -> myKeys `M.union` keys defaultConfig c
         , mouseBindings      = myMouseBindings
         }
         where
             tiled = Tall 1 (3%100) (1%2)
 
--- need to make sure we are on correct workspace and that it has no existing clients
--- also need to spawn terminals and firefox
+-- need to make sure we are on correct workspace and that it has no existing
+-- clients also need to spawn terminals and firefox
 startup :: X ()
 startup = do
           setWMName "LG3D"
-          sendMessage $ JumpToLayout "Grid False"
+          sendMessage $ JumpToLayout "Hinted Grid"
 
 myManageHook = composeAll . concat $
     [ [ className =? c --> doCenterFloat | c <- myFloatClasses]
     , [ title     =? t --> doCenterFloat | t <- myFloatTitles]
     , [ fmap ( "Page Info" `isInfixOf`) title --> doCenterFloat]
-    , [ className =? "URxvt" --> (ask >>= \w -> liftX (setOpacity w 0xd9999998) >> idHook)]
+    , [ className =? "URxvt" -->
+            (ask >>= \w -> liftX (setOpacity w 0x99999998) >> idHook)]
     , [ className =? "Shiretoko" --> doShift "2:web"]
     , [ className =? "XDvi" --> doShift "3:doc"]
     , [ className =? "Epdfview" --> doShift "3:doc"]
@@ -93,10 +105,10 @@ myManageHook = composeAll . concat $
     , [ className =? "OpenOffice.org 3.1" --> doShift "3:doc"]
     ]
     where
-        myFloatClasses = [ "MPlayer", "feh", "Xmessage", "Grip", "DClock", "Gimp"
-                         , "XDosEmu", "Nitrogen", "Nvidia-settings", "VirtualBox"
-                         , "Sonata", "xine", "Blender:Render", "Vdesk.py"
-                         , "org-scilab-modules-jvm-Scilab"
+        myFloatClasses = [ "MPlayer", "feh", "Xmessage", "Grip", "DClock"
+                         , "Gimp", "XDosEmu", "Nitrogen", "Nvidia-settings"
+                         , "VirtualBox", "Sonata", "xine", "Blender:Render"
+                         , "Vdesk.py", "org-scilab-modules-jvm-Scilab"
                          ]
 
         myFloatTitles = ["Downloads", "Add-ons", "Shiretoko Preferences"
@@ -110,13 +122,16 @@ myKeys = M.fromList $
     , ((mod4Mask                , xK_Return ), dwmpromote)
     , ((mod4Mask                , xK_b      ), sendMessage ToggleStruts)
     , ((mod4Mask                , xK_g      ), goToSelected defaultGSConfig)
-    , ((mod4Mask                , xK_r      ), spawnSelected defaultGSConfig ["gimp", "epdfview", "soffice"])
+    , ((mod4Mask                , xK_r      ),
+            spawnSelected defaultGSConfig ["gimp", "epdfview", "soffice"])
     , ((mod4Mask .|. controlMask, xK_f      ), spawn cmdBrowser)
     , ((mod4Mask .|. controlMask, xK_e      ), spawn cmdPizza1)
     , ((mod4Mask .|. controlMask, xK_d      ), spawn cmdPizza2)
     , ((mod4Mask .|. controlMask, xK_n      ), spawn "nitrogen ~/images")
-    , ((mod4Mask                , xK_x      ), scratchpadSpawnActionTerminal "urxvt")
-    , ((mod4Mask                , xK_s      ), submap $ searchEngineMap $ promptSearch mySPConfig)
+    , ((mod4Mask                , xK_x      ),
+            scratchpadSpawnActionTerminal "urxvt")
+    , ((mod4Mask                , xK_s      ),
+            submap $ searchEngineMap $ promptSearch mySPConfig)
     --, ((mod4Mask                , xK_s      ), )
     , ((0                       , 0x1008FF11), spawn cmdVolumeDown)
     , ((0                       , 0x1008FF12), spawn cmdVolumeMute)
@@ -156,7 +171,7 @@ myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
     ]
 
 -- PP config
-myPP h = defaultPP
+myPP h sc = defaultPP
     { ppCurrent         = dzenColor "#000000" color0
     , ppVisible         = dzenColor "grey70" "#404040"
     , ppHidden          = dzenColor color0 "#000000"
@@ -168,7 +183,9 @@ myPP h = defaultPP
     , ppLayout          = dzenColor color0 "black"
     , ppOrder           = id
     , ppOutput          = hPutStrLn h
-    , ppSort            = fmap (.scratchpadFilterOutWorkspace) getSortByXineramaRule
+    , ppSort            = fmap (.scratchpadFilterOutWorkspace) $
+                               if sc == 1 then getSortByIndex
+                                          else getSortByXineramaRule
     , ppExtras          = []
     }
 
