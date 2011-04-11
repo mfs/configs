mfs/configs

commit e0c11811ea5d581b5e96207cefa629a137b00fc2

[bashrc] Add router detection to PS1.

bashrc
@@ -28,6 +28,12 @@ function hg_ps1 {
     fi
 }
 
+function router_ps1 {
+    if [[ $( </proc/sys/net/ipv4/ip_forward ) == 1 ]]; then
+        echo "-[0]- "
+    fi
+}
+
 function set_prompt {
     local BLA='\[\033[0;30m\]' # BLACK
     local DGR='\[\033[1;30m\]' # DGRAY
@@ -65,7 +71,8 @@ function set_prompt {
     fi
 
     # need to fix \[ \] around color code for exit status
-    PS1="${RED}$chroot_name${LGR}\u${WHI}@${LGR}\h \$(exit_status \$?) "
+    PS1="${RED}\$(router_ps1)"
+    PS1="${PS1}${RED}$chroot_name${LGR}\u${WHI}@${LGR}\h \$(exit_status \$?) "
     PS1="${PS1}${PUR}${tty} ${CYA}\w ${git_p}${hg_p}\n"
     PS1="${PS1}${CYA}>>> ${NOR}"
 

commit 02fde201eab85e6cf75b1e2ad6c97a2b853a493e

[bashrc] Fix display of exit code.
This was broken in e0c11811ea5d581b5e96207cefa629a137b00fc2

bashrc
@@ -26,12 +26,6 @@ function hg_ps1 {
     fi
 }
 
-function router_ps1 {
-    if [[ $( </proc/sys/net/ipv4/ip_forward ) == 1 ]]; then
-        echo "-[0]- "
-    fi
-}
-
 function set_prompt {
     local BLA='\[\033[0;30m\]' # BLACK
     local DGR='\[\033[1;30m\]' # DGRAY
@@ -54,6 +48,12 @@ function set_prompt {
     local tty=$( tty )
     tty=${tty:5}
 
+    if [[ $( </proc/sys/net/ipv4/ip_forward ) == 1 ]]; then
+        router_p="-[0]- "
+    else
+        router_p=""
+    fi
+
     if [ -r /etc/chroot ]; then
         chroot_name="($(cat /etc/chroot))"
     elif [ -r /etc/debian_chroot ]; then
@@ -69,7 +69,7 @@ function set_prompt {
     fi
 
     # need to fix \[ \] around color code for exit status
-    PS1="${RED}\$(router_ps1)"
+    PS1="${RED}$router_p"
     PS1="${PS1}${RED}$chroot_name${LGR}\u${WHI}@${LGR}\h \$(exit_status \$?) "
     PS1="${PS1}${PUR}${tty} ${CYA}\w ${git_p}${hg_p}\n"
     PS1="${PS1}${CYA}>>> ${NOR}"