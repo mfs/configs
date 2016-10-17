5d44ec6f52769b3ee7967161e5c321c5fbf5903b
Mike Sampson <mike@sambodata.com>
2016-10-17 23:08:53 +1100

    Fix colors.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,6 +3,15 @@ import XMonad.Layout.Spacing
 import XMonad.Layout.ThreeColumns
 import XMonad.Hooks.DynamicLog
 
+colorBlack   = "#1d1f21"
+colorRed     = "#cc6666"
+colorGreen   = "#b5bd68"
+colorYellow  = "#f0c674"
+colorBlue    = "#81a2be"
+colorMagenta = "#b294bb"
+colorCyan    = "#8abeb7"
+colorWhite   = "#c5c8c6"
+
 layout =  spacing 12 (three ||| tiled ||| Mirror tiled ||| Full)
 	where
 		three = ThreeColMid nmaster delta (1/3)
@@ -11,11 +20,21 @@ layout =  spacing 12 (three ||| tiled ||| Mirror tiled ||| Full)
 		ratio = 1/2
 		delta = 3/100
 
-main = xmonad =<< xmobar defaultConfig
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+myPP = xmobarPP
+	{ ppCurrent         = xmobarColor colorBlack colorBlue
+	, ppVisible         = xmobarColor colorWhite "#404040"
+	, ppHidden          = xmobarColor colorWhite colorBlack
+	, ppHiddenNoWindows = xmobarColor colorWhite colorBlack
+	, ppSep             = xmobarColor colorBlue colorBlack " | "
+	, ppTitle           = xmobarColor colorGreen colorBlack
+	}
+
+main = xmonad =<< statusBar "xmobar" myPP toggleStrutsKey defaultConfig
 	{ modMask            = mod4Mask
 	, borderWidth        = 1
 	, terminal           = "st"
 	, layoutHook         = layout
-	, logHook            = dynamicLog
 	, normalBorderColor  = "#6f6f6f"
-	, focusedBorderColor = "#b5bd68" }
+	, focusedBorderColor = colorGreen }
